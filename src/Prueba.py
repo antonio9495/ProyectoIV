1bbabde738cfaf62f961da73807ce05675ede93c
Antonio Jimenez Argüello <antoniojimenez@correo.ugr.es>
2018-11-04 19:34:54 +0100

    añadiendo organizacion

---
+++ src/Prueba.py
@@ -0,0 +1,76 @@
+import json
+
+class Prueba:
+    datos=[]
+
+    def __init__(self, ruta):
+        with open(ruta) as f:
+            self.datos = json.load(f)
+
+    def getInfoDato(self, id):
+        if(id in self.datos):
+            info = "Información completa del dato: " + id +"\n"
+            info += "Información de la actividad :\n"
+            info += "\tIdentificador de actividad: " +  self.datos[id]["Actividad"]["idA"]
+            info += "\n\tTitulo de la actividad: " + self.datos[id]["Actividad"]["titulo"]
+            info += "\n\tTipo de la actividad: " + self.datos[id]["Actividad"]["tipo"]
+            info += "\n\tPosiciones asociada: "
+            for a in (self.PosicionesAsociadasAactividades(self.datos[id]["Actividad"]["idA"]))[1]:
+                info += a
+                info += " ,"
+            info += "\nInformación de la posición :\n"
+            info += "\tIdentificador de la posición: " +  self.datos[id]["Posicion"]["idP"]
+            info += "\n\tTitulo de la actividad: " + self.datos[id]["Posicion"]["latitud"]
+            info += "\n\tTipo de la actividad: " + self.datos[id]["Posicion"]["longitud"]
+            info += "\n\tActividades asociada: "
+            for a in (self.ActividadesAsociadasAposicion(self.datos[id]["Posicion"]["idP"]))[1]:
+                info += a
+                info += " ,"
+            info+="\n"
+        else:
+            info ="No existe un objeto con ese id"
+        return info
+
+    def buscarPosicion(self, idp):
+        for a in self.datos:
+            if(self.datos[a]["Posicion"]["idP"] == idp):
+                return (True, self.datos[a]["Posicion"])
+        return (False, None)
+
+    def buscarActividad(self, ida):
+        for a in self.datos:
+            if(self.datos[a]["Actividad"]["idA"] == ida):
+                return (True, self.datos[a]["Actividad"])
+        return (False, None)
+
+    def ActividadesAsociadasAposicion(self, idp):
+        posiciones = []
+        if ( (self.buscarPosicion(idp))[0] == True ):
+            for a in self.datos:
+                if(self.datos[a]["Posicion"]["idP"] == idp):
+                    posiciones.append(self.datos[a]["Actividad"]["idA"])
+            return (True, posiciones)
+        else:
+            return (False, None)
+
+    def PosicionesAsociadasAactividades(self, ida):
+        posiciones = []
+        if ( (self.buscarActividad(ida))[0] == True ):
+            for a in self.datos:
+                if(self.datos[a]["Actividad"]["idA"] == ida):
+                    posiciones.append(self.datos[a]["Posicion"]["idP"])
+            return (True, posiciones)
+        else:
+            return (False, None)
+
+    def crearObjeto(self, actividad, coordenadas):
+        id = -1
+        dictA ={"idA": "None", "tipo": "None", "titulo": "None" }
+        dictP ={"idP": "None","latitud": "None","longitud": "-None"}
+
+        if( type(actividad) == dict and type(coordenadas) == dict):
+            if(dictA.keys() == actividad.keys() and dictP.keys() == coordenadas.keys()):
+                id = "idobjeto"+ str(len(self.datos)+1)
+                self.datos[id] = {"Posicion": {"idP":coordenadas["idP"],"latitud":coordenadas["latitud"],"longitud":coordenadas["longitud"]},
+                                    "Actividad": {"idA":actividad["idA"],"tipo":actividad["tipo"],"titulo":actividad["titulo"]}}
+        return id
---
+++ src/test_prueba.py
@@ -0,0 +1,30 @@
+from Prueba import Prueba
+
+p = Prueba("../data/datos.json")
+
+def testInicio():
+    assert type(p.datos["idobjeto1"]) is dict
+
+def testGetInfoDato():
+    assert p.getInfoDato("noexiste") == "No existe un objeto con ese id", "correcto"
+    assert p.getInfoDato("idobjeto1") != "No existe un objeto con ese id", "correcto"
+
+def testBuscaDores():
+    assert (p.buscarPosicion("noexiste"))[0] == False, "no debe existir"
+    assert (p.buscarPosicion("Pos1"))[0] == True, "no debe existir"
+    assert (p.buscarActividad("noexiste"))[0] == False, "no debe existir"
+    assert (p.buscarActividad("Act1"))[0] == True, "no debe existir"
+
+def testAsociados():
+    assert (p.ActividadesAsociadasAposicion("noexiste"))[0] == False, "no existe la posicion"
+    assert (p.ActividadesAsociadasAposicion("Pos1"))[0] == True, "Existe la posicion"
+    assert len((p.ActividadesAsociadasAposicion("Pos1"))[1]) == 1, "La cantidad es correcta"
+    assert (p.PosicionesAsociadasAactividades("noexiste"))[0]== False, "no existe la actividad"
+    assert (p.PosicionesAsociadasAactividades("Act1"))[0] == True, "no existe la posicion"
+    assert len((p.PosicionesAsociadasAactividades("Act1"))[1]) == 2, "La cantidad es correcta"
+
+def testCrearObjeto():
+    id = p.crearObjeto({"idA": "Act1", "tipo": "Deporte", "titulo": "Ir al gimnasio" }, {"idP": "Pos3","latitud": "37.197222","longitud": "-3.623889"})
+    assert (id==("idobjeto"+ str(len(p.datos)))), "se ha creado bien"
+    id = p.crearObjeto({"idP": "Act1", "tipo": "Deporte", "titulo": "Ir al gimnasio" }, {"idP": "Pos3","latitud": "37.197222","longitud": "-3.623889"})
+    assert (id == -1), "No se ha podido crear el objeto"
