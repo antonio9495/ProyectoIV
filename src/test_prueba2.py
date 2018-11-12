12aaddc701ed232a2b6ce88b318feee262a943a4
Antonio Jimenez Argüello <antoniojimenez@correo.ugr.es>
2018-11-12 18:01:50 +0100

    arreglo

--- src/test_prueba2.py
+++ src/test_prueba2.py
@@ -1,7 +1,8 @@
 import json, requests
 from requests import *
 
-url ="https://mysterious-bastion-92654.herokuapp.com/"
+url = "https://mysterious-bastion-92654.herokuapp.com/"
+#url ="http://0.0.0.0:5000/"
 
 def testInicio():
     response = requests.get(url)
@@ -17,7 +18,7 @@ def testObtenerDatosObjeto():
 def testObtenerPosicion():
     response = requests.get(url+"searchP/1")
     assert response.status_code == 200, "Devuelve codigo correcto."
-    assert response.json() == 'No existe una posición con ese valor.', "Devuelve estado correcto."
+    assert response.json() == 'No existe una posicion con ese valor.', "Devuelve estado correcto."
 
 def testObtenerActividad():
     response = requests.get(url+"searchA/1")
